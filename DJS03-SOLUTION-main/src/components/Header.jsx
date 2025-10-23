59c9d04bc8e928edc4c9ac6613d2ac5dfd7776af
Reneva <renevanewman7@gmail.com>
2025-10-23 18:02:36 +0300

    added new prop to component

--- DJS03-SOLUTION-main/src/components/Header.jsx
+++ DJS03-SOLUTION-main/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export default function Header({ onSearch }) {
+export default function Header({ onSearch, onSortChange }) {
   const [query, setQuery] = useState("");
 
   const handleChange = (e) => {
