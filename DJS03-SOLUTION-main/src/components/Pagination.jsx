1b4249c5f39a5dd868679352c753b77b3a4e238b
Reneva <renevanewman7@gmail.com>
2025-10-23 18:28:39 +0300

    added new file and code for pagination

---
+++ DJS03-SOLUTION-main/src/components/Pagination.jsx
@@ -0,0 +1,45 @@
+// src/components/Pagination.jsx
+import React from "react";
+
+export default function Pagination({ 
+  totalPages, 
+  currentPage, 
+  onPageChange 
+}) {
+  const pageNumbers = [];
+  for (let i = 1; i <= totalPages; i++) {
+    pageNumbers.push(i);
+  }
+
+  return (
+    <div className="flex justify-center space-x-2 p-4">
+      <button
+        onClick={() => onPageChange(currentPage - 1)}
+        disabled={currentPage === 1}
+        className="px-3 py-1 bg-gray-200 rounded disabled:opacity-50"
+      >
+        Previous
+      </button>
+
+      {pageNumbers.map((num) => (
+        <button
+          key={num}
+          onClick={() => onPageChange(num)}
+          className={`px-3 py-1 rounded ${
+            num === currentPage ? "bg-blue-500 text-white" : "bg-gray-200"
+          }`}
+        >
+          {num}
+        </button>
+      ))}
+
+      <button
+        onClick={() => onPageChange(currentPage + 1)}
+        disabled={currentPage === totalPages}
+        className="px-3 py-1 bg-gray-200 rounded disabled:opacity-50"
+      >
+        Next
+      </button>
+    </div>
+  );
+}
